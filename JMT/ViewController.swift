89f49709598df4a7ad0d2f15bb902b9a054832c1
wonchanjoo <wcj0911@naver.com>
2023-06-01 14:54:26 +0900

    create new project

---
+++ JMT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JMT
+//
+//  Created by 원찬주 on 2023/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
